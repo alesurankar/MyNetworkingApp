981b384bafe8ce9fb5a8fc754a016e6bf9c146a6
ales2026 <ale.urankar@gmail.com>
2026-04-10 15:14:13 +0200

    Client: add App class

---
+++ Client/App..cpp
@@ -0,0 +1,21 @@
+#include "App.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+
+App::App(std::atomic<bool>& runFlag)
+	:
+	running_(runFlag)
+{
+}
+
+App::~App()
+{
+}
+
+void App::Run()
+{
+	std::cout << "Client is running...\n";
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+}
