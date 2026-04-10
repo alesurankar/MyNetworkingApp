3d061e66a5a74d956d178982c09750fd8a323a08
ales2026 <ale.urankar@gmail.com>
2026-04-10 11:36:59 +0200

    change executable location and now able to open server or client

--- MyNetworkingApp/Main.cpp
+++ MyNetworkingApp/Main.cpp
@@ -9,13 +9,18 @@ int main()
     char c;
     std::cin >> c;
 
-    if (c == 's') {
-        bp::child server("./Server.exe");
-        server.wait();
+    try {
+        if (c == 's') {
+            bp::child server("../Server/bin/Debug/Server.exe");
+            server.wait();
+        }
+        else if (c == 'c') {
+            bp::child client("../Client/bin/Debug/Client.exe");
+            client.wait();
+        }
     }
-    else if (c == 'c') {
-        bp::child client("./Client.exe");
-        client.wait();
+    catch (const std::exception& e) {
+        std::cout << "Error: " << e.what() << "\n";
     }
     return 0;
 }
