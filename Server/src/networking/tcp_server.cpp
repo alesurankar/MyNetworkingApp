9bc18d20a2ad3c010d5bbbeb8f3c0cc0e2e99e5b
ales2026 <ale.urankar@gmail.com>
2026-04-11 15:40:13 +0200

    server: add session

--- Server/src/networking/tcp_server.cpp
+++ Server/src/networking/tcp_server.cpp
@@ -3,19 +3,20 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/ip/address.hpp>
+#include <boost/system/detail/error_code.hpp>
 
 #include <string_view>
 #include <cstdint>
 #include <iostream>
+#include <utility>
+#include <memory>
 
+using error_code = boost::system::error_code;
 
+// Server
 TcpServer::TcpServer(asio::io_context& io_context, std::string_view address, uint16_t port)
 	:
 	acceptor_(io_context, tcp::endpoint(asio::ip::make_address(address), port))
-{
-}
-
-TcpServer::~TcpServer()
 {
 }
 
@@ -26,4 +27,40 @@ void TcpServer::Accept()
         << ":"
         << acceptor_.local_endpoint().port()
         << "\n";
+    acceptor_.async_accept([this](error_code ec, tcp::socket socket) {
+        if (!ec) {
+            auto session = std::make_unique<Session>(std::move(socket));
+            session->Start();
+            Join(std::move(session));
+        }
+        else {
+            std::cerr << "Accept failed: " << ec.message() << "\n";
+        }
+        Accept(); 
+        });
+}
+
+void TcpServer::Join(std::unique_ptr<Session> client_session)
+{
+    std::cout << "TcpServer::Join called";
+}
+
+void TcpServer::Leave(std::unique_ptr<Session> client_session)
+{
+}
+
+
+
+// Session
+Session::Session(tcp::socket socket)
+{
+    std::cout << "Session Constructor called";
+}
+
+void Session::Start()
+{
+}
+
+void Session::Stop()
+{
 }
--- Server/src/networking/tcp_server.hpp
+++ Server/src/networking/tcp_server.hpp
@@ -4,17 +4,33 @@
 
 #include <string_view>
 #include <cstdint>
+#include <unordered_set>
+#include <memory>
 
 
 namespace asio = boost::asio;
 using tcp = asio::ip::tcp;
 
+class Session;
+
 class TcpServer
 {
 public:
 	TcpServer(asio::io_context& io_context, std::string_view address, uint16_t port);
-	~TcpServer();
 	void Accept();
+	void Leave(std::unique_ptr<Session> client_session);
+private:
+	void Join(std::unique_ptr<Session> client_session);
 private:
 	tcp::acceptor acceptor_;
+	std::unordered_set<std::unique_ptr<Session>> active_session;
+};
+
+class Session
+{
+public:
+	Session(tcp::socket socket);
+	void Start();
+	void Stop();
+private:
 };
