24ad4d3d742895dd7ade1bc824f39c3debe9c557
ales2026 <ale.urankar@gmail.com>
2026-04-13 07:01:49 +0200

    server: name consistancy in messageHandler

--- Server/src/core/message_handler.cpp
+++ Server/src/core/message_handler.cpp
@@ -1,6 +1,5 @@
 #include "message_handler.hpp"
 
-//#include <iostream>
 #include <mutex>
 #include <string>
 
@@ -9,44 +8,38 @@ MessageHandler::MessageHandler()
 {
 }
 
-
 void MessageHandler::ServerToMSG(const std::string& message)
 {
-	std::lock_guard<std::mutex> lock(IN_mtx);
-	app_messages.push(message);
-	//std::cout << "void MessageHandler::ServerToMSG(const std::string& message): " << message << " pushed to queue\n";
+	std::lock_guard<std::mutex> lock(mtxIn_);
+	app_messages_.push(message);
 }
 
 std::string MessageHandler::MSGToApp()
 {
 	{
-		std::lock_guard<std::mutex> lock(IN_mtx);
-		if (!app_messages.empty())
-		{
-			msg = app_messages.front();
-			app_messages.pop();
+		std::lock_guard<std::mutex> lock(mtxIn_);
+		if (!app_messages_.empty()) {
+			msg_ = app_messages_.front();
+			app_messages_.pop();
 		}
-		else
-		{
-			msg.clear();
+		else {
+			msg_.clear();
 		}
 	}
-	return msg;
+	return msg_;
 }
 
 void MessageHandler::AppToMSG(const std::string& response)
 {
-	std::lock_guard<std::mutex> lock(OUT_mtx);
-	validResponse = response;
-	//std::cout << "void MessageHandler::AppToMSG(const std::string& response): " << validResponse << "-waiting to be read\n";
-	//std::cout << "void MessageHandler::AppToMSG(const std::string& response): " << response << "-pushed to app_responses\n";
+	std::lock_guard<std::mutex> lock(mtxOut_);
+	validResponse_ = response;
 }
 
 std::string MessageHandler::MSGToServer()
 {
-	std::lock_guard<std::mutex> lock(OUT_mtx);
-	std::string messageToServer = validResponse + "\n";
-	validResponse.clear();
-	//std::cout << "std::string MessageHandler::MSGToServer(): " << messageToServer << "-was read\n";
+	std::lock_guard<std::mutex> lock(mtxOut_);
+	std::string messageToServer = validResponse_ + "\n";
+	validResponse_.clear();
+
 	return messageToServer;
 }
--- Server/src/core/message_handler.hpp
+++ Server/src/core/message_handler.hpp
@@ -13,9 +13,9 @@ public:
 	void AppToMSG(const std::string& response);
 	std::string MSGToServer();
 private:
-	std::mutex IN_mtx;
-	std::mutex OUT_mtx;
-	std::string msg;
-	std::string validResponse;
-	std::queue<std::string> app_messages;
+	std::mutex mtxIn_;
+	std::mutex mtxOut_;
+	std::string msg_;
+	std::string validResponse_;
+	std::queue<std::string> app_messages_;
 };
