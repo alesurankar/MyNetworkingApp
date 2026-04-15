310263b99f1e2f46bddbba0449a2181bbc57970d
ales2026 <ale.urankar@gmail.com>
2026-04-15 08:32:17 +0200

    client: simplify app

--- Client/src/app/app..cpp
+++ Client/src/app/app..cpp
@@ -1,14 +1,12 @@
 #include "app.hpp"
 #include <core/message_handler.hpp>
 
-#include <iostream>
 #include <thread>
 #include <chrono>
 #include <atomic>
 #include <memory>
 #include <utility>
 #include <string>
-#include <mutex>
 
 
 App::App(std::atomic<bool>& running, std::shared_ptr<MessageHandler> msgHandler)
@@ -17,16 +15,12 @@ App::App(std::atomic<bool>& running, std::shared_ptr<MessageHandler> msgHandler)
 	msgHandler_(std::move(msgHandler))
 {
 	inputThread_ = std::thread(&App::InputLoop, this);
-	updateThread_ = std::thread(&App::UpdateLoop, this);
 }
 
 App::~App()
 {
 	if (inputThread_.joinable()) {
 		inputThread_.join();
-	}
-	if (updateThread_.joinable()) {
-		updateThread_.join();
 	}
 }
 
@@ -36,58 +30,16 @@ App::~App()
 void App::InputLoop()
 {
 	while (running_.load()) {
-		CaptureInput();
-		SetMessageForMSG();
+		std::string msg = CaptureInput();
+		if (!msg.empty()) {
+			msgHandler_->AppToHandler(msg);
+		}
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 }
 
-void App::CaptureInput()
+std::string App::CaptureInput()
 {
-}
-
-void App::SetMessageForMSG()
-{
-	std::string msgOut;
-	std::lock_guard<std::mutex> lock(mtxOut_);
-	if (!outgoingMessages_.empty()) {
-		msgOut = outgoingMessages_.front();
-		outgoingMessages_.pop();
-		msgHandler_->AppToHandler(msgOut);
-		std::cout << "Step8. '" << msgOut << "' poped from queue... App::SetMessageForMSG (main thread)\n";
-	}
-}
-
-
-///////////////////////////////////////////////
-// Update thread
-void App::UpdateLoop()
-{
-	while (running_.load()) {
-		TakeFromQueue();
-		PushToQueue();
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	}
-}
-
-void App::TakeFromQueue()
-{
-	std::lock_guard<std::mutex> lock(mtxIn_);
-	if (!incomingMessages_.empty()) {
-		message_ = incomingMessages_.front();
-		incomingMessages_.pop();
-		std::cout << "Step5. '" << message_ << "' poped from queue... App::TakeFromQueue (update thread)\n";
-	}
-}
-
-void App::PushToQueue()
-{
-	std::lock_guard<std::mutex> lock(mtxOut_);
-	if (!response_.empty()) {
-		outgoingMessages_.push(response_);
-		std::cout << "Step7. '" << response_ << "' pushed to queue... App::PushToQueue (update thread)\n";
-		response_.clear();
-	}
 }
 
 
@@ -95,21 +47,13 @@ void App::PushToQueue()
 // Main thread
 void App::Run()
 {
-	GetMessageFromMSG();
-	ShowOutput();
+	std::string msg = msgHandler_->HandlerToApp();
+	if (!msg.empty()) {
+		ShowOutput(msg);
+	}
 	std::this_thread::sleep_for(std::chrono::milliseconds(10));
 }
 
-void App::GetMessageFromMSG()
-{
-	std::string msgIn = msgHandler_->HandlerToApp();
-	std::lock_guard<std::mutex> lock(mtxIn_);
-	if (!msgIn.empty()) {
-		incomingMessages_.push(msgIn);
-		std::cout << "Step4. '" << msgIn << "' pushed to queue... App::GetMessageFromMSG (main thread)\n";
-	}
-}
-
-void App::ShowOutput()
+void App::ShowOutput(const std::string& msg)
 {
 }
--- Client/src/app/app.hpp
+++ Client/src/app/app.hpp
@@ -1,9 +1,7 @@
 #pragma once
 #include <atomic>
 #include <memory>
-#include <queue>
 #include <string>
-#include <mutex>
 #include <thread>
 
 class MessageHandler;
@@ -13,30 +11,13 @@ class App
 public:
 	App(std::atomic<bool>& running, std::shared_ptr<MessageHandler> msgHandler);
 	~App();
-private:
-	// Input thread
-	void InputLoop();
-	void CaptureInput();
-	void SetMessageForMSG();
-	// Update thread
-	void UpdateLoop();
-	void TakeFromQueue();
-	void PushToQueue();
-	// Main thread
-public:
 	void Run();
 private:
-	void GetMessageFromMSG();
-	void ShowOutput();
+	void InputLoop();
+	std::string CaptureInput();
+	void ShowOutput(const std::string& msg);
 private:
 	std::atomic<bool>& running_;
-	std::string message_;
-	std::string response_;
 	std::thread inputThread_;
-	std::thread updateThread_;
-	std::mutex mtxIn_;
-	std::mutex mtxOut_;
 	std::shared_ptr<MessageHandler> msgHandler_;
-	std::queue<std::string> incomingMessages_;
-	std::queue<std::string> outgoingMessages_;
 };
