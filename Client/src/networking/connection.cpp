edcfe4ac2a8b1938bd62c9d099ad40c8f82e8f6b
ales2026 <ale.urankar@gmail.com>
2026-04-15 06:13:11 +0200

    client: add Connection class

---
+++ Client/src/networking/connection.cpp
@@ -0,0 +1,114 @@
+#include "connection.hpp"
+#include <core/message_handler.hpp>
+
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/post.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/write.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/asio/buffer.hpp>
+
+#include <iostream>
+#include <utility>
+#include <string>
+#include <memory>
+#include <istream>
+#include <chrono>
+
+
+using error_code = boost::system::error_code;
+
+Connection::Connection(tcp::socket socket, std::shared_ptr<MessageHandler> msgHandler)
+	:
+	socket_(std::move(socket)),
+    timer_(socket_.get_executor()),
+    msgHandler_(std::move(msgHandler))
+{
+
+}
+
+void Connection::Start()
+{
+    DoRead();
+
+    auto self = shared_from_this();
+    asio::post(socket_.get_executor(),
+        [this, self]()
+        {
+            DoWrite();
+        });
+}
+
+void Connection::Stop()
+{
+    error_code ec;
+
+    socket_.cancel(ec);
+    socket_.shutdown(tcp::socket::shutdown_both, ec);
+    socket_.close(ec);
+}
+
+void Connection::Send(const std::string& msg)
+{
+    msgHandler_->AppToHandler(msg);
+
+    auto self = shared_from_this();
+    asio::post(socket_.get_executor(),
+        [this, self]()
+        {
+            DoWrite();
+        });
+}
+
+void Connection::DoRead()
+{
+    auto self = shared_from_this();
+    asio::async_read_until(socket_, buffer_, '\n',
+        [this, self](error_code ec, std::size_t) {
+            if (!ec) {
+                std::istream is(&buffer_);
+                std::string msg;
+                std::getline(is, msg);
+
+                std::cout << "\nStep1. '" << msg << "' received from network... Connection::DoRead (networking thread)\n";
+                if (!msg.empty()) {
+                    msgHandler_->NetToHandler(msg);
+                }
+                DoRead();
+            }
+            else {
+                Stop();
+            }
+        });
+}
+
+void Connection::DoWrite()
+{
+    auto self = shared_from_this();
+    std::string resp = msgHandler_->HandlerToNet();
+
+    if (resp.empty()) {
+        timer_.expires_after(std::chrono::milliseconds(10));
+        timer_.async_wait([this, self](error_code ec) {
+            if (!ec) {
+                DoWrite();
+            }
+            });
+        return;
+    }
+
+    resp.push_back('\n');
+    auto msg = std::make_shared<std::string>(std::move(resp));
+
+    asio::async_write(socket_, asio::buffer(*msg),
+        [this, self, msg](const error_code& ec, std::size_t) {
+            if (!ec) {
+                std::cout << "Step10.'" << *msg << "' sent to network... Connection::DoWrite\n";
+                DoWrite();
+            }
+            else {
+                Stop();
+            }
+        });
+}
---
+++ Client/src/networking/connection.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#define _WIN32_WINNT 0x0A00
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <boost/asio/steady_timer.hpp>
+
+#include <memory>
+#include <deque>
+#include <string>
+
+
+namespace asio = boost::asio;
+using tcp = asio::ip::tcp;
+
+class MessageHandler;
+
+class Connection : public std::enable_shared_from_this<Connection>
+{
+public:
+    Connection(tcp::socket socket, std::shared_ptr<MessageHandler> msgHandler);
+    void Start();
+    void Stop();
+    void Send(const std::string& msg);
+private:
+    void DoRead();
+    void DoWrite();
+private:
+    tcp::socket socket_;
+    asio::streambuf buffer_;
+    asio::steady_timer timer_;
+    std::deque<std::string> writeQueue_;
+    std::shared_ptr<MessageHandler> msgHandler_;
+};
