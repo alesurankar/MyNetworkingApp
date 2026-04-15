fe6bf174c5e75623e093d71be74d9c4ae3eb68bb
ales2026 <ale.urankar@gmail.com>
2026-04-15 09:14:07 +0200

    server: simplify app

--- Server/src/app/app.cpp
+++ Server/src/app/app.cpp
@@ -8,95 +8,44 @@
 #include <memory>
 #include <utility>
 #include <string>
-#include <mutex>
 
 
 App::App(std::atomic<bool>& running, std::shared_ptr<MessageHandler> msgHandler)
 	:
 	running_(running),
-	msgHandler_(std::move(msgHandler)),
-	nextFrame_(true)
-{
-	updateThread_ = std::thread(&App::UpdateLoop, this);
-}
-
-App::~App()
-{
-	if (updateThread_.joinable()) {
-		updateThread_.join();
-	}
-}
+	msgHandler_(std::move(msgHandler))
+{}
 
 void App::Run()
 {
-	if (nextFrame_.load()) {
-		GetMessageFromMSG();
-		SetMessageForMSG();
-		nextFrame_.store(false);
+	TakeInput();
+	Process();
+	PushOutput();
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+}
+
+void App::TakeInput()
+{
+	input_ = msgHandler_->HandlerToApp();
+	if (!input_.empty()) {
+		std::cout << "Received from network: " << input_ << "\n";
 	}
 }
 
-void App::GetMessageFromMSG()
+void App::Process()
 {
-	std::string msgIn = msgHandler_->HandlerToApp();
-	std::lock_guard<std::mutex> lock(mtxIn_);
-	if (!msgIn.empty()) {
-		msgToUpdate_.push(msgIn);
-		std::cout << "Step4. '"<< msgIn << "' pushed to queue... App::GetMessageFromMSG (main thread)\n";
+	// TODO : Update parameters based on the message
+	if (!input_.empty()) {
+		output_ = input_ + " is processed.";
+		input_.clear();
 	}
 }
 
-void App::SetMessageForMSG()
+void App::PushOutput()
 {
-	std::string msgOut;
-	std::lock_guard<std::mutex> lock(mtxOut_);
-	if (!msgIsUpdated_.empty()) {
-		msgOut = msgIsUpdated_.front();
-		msgIsUpdated_.pop();
-		msgHandler_->AppToHandler(msgOut);
-		std::cout << "Step8. '" << msgOut << "' poped from queue... App::SetMessageForMSG (main thread)\n";
-	}
-}
-
-
-///////////////////////////////////////////////
-void App::UpdateLoop()
-{
-	while (running_.load()) {
-		TakeFromQueue();
-		UpdateParameters();
-		PushToQueue();
-
-		nextFrame_.store(true);
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-	}
-}
-
-void App::TakeFromQueue()
-{
-	std::lock_guard<std::mutex> lock(mtxIn_);
-	if (!msgToUpdate_.empty()) {
-		message_ = msgToUpdate_.front();
-		msgToUpdate_.pop();
-		std::cout << "Step5. '" << message_ << "' poped from queue... App::TakeFromQueue (update thread)\n";
-	}
-}
-
-void App::UpdateParameters()
-{
-	if (!message_.empty()) {
-		// TODO : Update parameters based on the message
-		response_ = message_ + " is processed.";
-		message_.clear();
-	}
-}
-
-void App::PushToQueue()
-{
-	std::lock_guard<std::mutex> lock(mtxOut_);
-	if (!response_.empty()) {
-		msgIsUpdated_.push(response_);
-		std::cout << "Step7. '" << response_ << "' pushed to queue... App::PushToQueue (update thread)\n";
-		response_.clear();
+	if (!output_.empty()) {
+		std::cout << "Sending to network: " << output_ << "\n";
+		msgHandler_->AppToHandler(std::move(output_));
+		output_.clear();
 	}
 }
--- Server/src/app/app.hpp
+++ Server/src/app/app.hpp
@@ -1,10 +1,8 @@
 #pragma once
 #include <atomic>
 #include <memory>
-#include <queue>
 #include <string>
-#include <thread>
-#include <mutex>
+
 
 class MessageHandler;
 
@@ -12,24 +10,14 @@ class App
 {
 public:
 	App(std::atomic<bool>& running, std::shared_ptr<MessageHandler> msgHandler);
-	~App();
 	void Run();
 private:
-	void GetMessageFromMSG();
-	void SetMessageForMSG();
-	void UpdateLoop();
-	void TakeFromQueue();
-	void UpdateParameters();
-	void PushToQueue();
+	void TakeInput();
+	void Process();
+	void PushOutput();
 private:
 	std::atomic<bool>& running_;
-	std::atomic<bool> nextFrame_;
-	std::string message_;
-	std::string response_;
-	std::thread updateThread_;
-	std::mutex mtxIn_;
-	std::mutex mtxOut_;
+	std::string input_;
+	std::string output_;
 	std::shared_ptr<MessageHandler> msgHandler_;
-	std::queue<std::string> msgToUpdate_;
-	std::queue<std::string> msgIsUpdated_;
 };
